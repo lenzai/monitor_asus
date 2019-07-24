36be756b74115e03b4516f1853c330156a8b7c89
Frederic Bazin <bazin.frederic@gmail.com>
2019-07-24 20:51:56 +0800

    adding router mac address

--- ap_mac.sh
+++ ap_mac.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "ip mac" | tee cache/ap_mac.txt
 for ip in `cat cache/ap_list.txt`; do
-  ifconfig = `ssh admin@$ip /sbin/ifconfig | grep -E "HWaddr (.*)"`
-  echo $ifconfig
-#   while read mac; do echo "$i"; done < "$ifconfig"
-# #  for mac in `ssh admin@$ip /sbin/ifconfig | grep -E "HWaddr (.*)"`; do
-#     echo $ip $mac | tee -a cache/ap_mac.txt
-#   done;
-done;
+   while read  mac; do
+     echo $ip $mac | tee -a cache/ap_mac.txt
+   done < <(ssh admin@$ip /sbin/ifconfig | grep -E "HWaddr (.*)" | sed  "s/Link encap:.* HWaddr//g") #(echo $ifconfig)
+done
--- client_data.sh
+++ client_data.sh
@@ -1,7 +1,7 @@
-
+#!/bin/bash
 for ip in `cat cache/ap_list.txt`; do
   for interface in `ssh admin@$ip /sbin/ifconfig | grep eth| tail -n2| cut -d " " -f 1`; do
-    grep $ip cache/ap_mac.txt
+    grep "$ip $interface" cache/ap_mac.txt
     ssh admin@$ip /usr/sbin/wl -i $interface assoclist | cut -d " " -f 2;
   done;
 done;
